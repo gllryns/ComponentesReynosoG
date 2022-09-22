a35b3231e28dabff8642f3226d3bc82e0656b185
gllryns <gllryns@gmail.com>
2022-09-21 21:49:07 -0300

    Agrega componentes menu, toolbar y contenido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
+import { ContenidoComponent } from './componentes/contenido/contenido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    ToolbarComponent,
+    ContenidoComponent
   ],
   imports: [
     BrowserModule
